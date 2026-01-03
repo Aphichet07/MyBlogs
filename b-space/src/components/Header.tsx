1551b478d5454f42495de90ce35fab5fe7bc0b57
Aphichet07 <aphichet254888@gmail.com>
2026-01-04 02:27:10 +0700

    chage header that suit with footer

--- b-space/src/components/Header.tsx
+++ b-space/src/components/Header.tsx
@@ -2,12 +2,13 @@ import '../App.css'
 
 function Header() {
   return (
-    <header className="bg-amber-50/80 backdrop-blur-md border-b border-amber-100 sticky top-0 z-50">
+    <header className="bg-white/80 backdrop-blur-md border-b border-slate-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           
           <div className="shrink-0 cursor-pointer">
-            <span className="pl-20 font-bold text-2xl text-slate-900 tracking-tight">
+             {/* Logo  */}
+            <span className="font-bold text-2xl text-slate-900 tracking-tight">
               B-Space
             </span>
           </div>
@@ -15,12 +16,12 @@ function Header() {
           <nav>
             <ul className="flex gap-8">
               <li>
-                <a href="#" className="text-sm font-medium text-slate-600 hover:text-amber-600 transition-colors duration-200">
+                <a href="#" className="text-sm font-medium text-slate-600 hover:text-blue-600 transition-colors duration-200">
                   Labs
                 </a>
               </li>
               <li>
-                <a href="#" className="text-sm font-medium text-slate-600 hover:text-amber-600 transition-colors duration-200">
+                <a href="#" className="text-sm font-medium text-slate-600 hover:text-blue-600 transition-colors duration-200">
                   Notebook
                 </a>
               </li>
